ed1308a038049b295be458d422074c849b0d695c
Mark Hoe4 <mhoe4@mailfence.com>
2022-07-08 01:44:44 -0400

    Updates

---
+++ precache-manifest.a2528e91997116ba54b625007d57ddf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6704f851b732986d6383187f12f168c7",
+    "url": "/mathpowerclay/index.html"
+  },
+  {
+    "revision": "43496a40b61e2ace959d",
+    "url": "/mathpowerclay/static/css/main.3b23da4d.chunk.css"
+  },
+  {
+    "revision": "1a59ff6a7b7766ccb374",
+    "url": "/mathpowerclay/static/js/2.47b36e1b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/mathpowerclay/static/js/2.47b36e1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43496a40b61e2ace959d",
+    "url": "/mathpowerclay/static/js/main.87cb4ad2.chunk.js"
+  },
+  {
+    "revision": "cdba6a610d45529ac351",
+    "url": "/mathpowerclay/static/js/runtime-main.18e1e102.js"
+  }
+]);
